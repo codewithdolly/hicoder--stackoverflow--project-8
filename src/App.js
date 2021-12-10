93e08c7a902ffca788a9fb6ada8ad8a786ed0ab1
hicoderindia <hicoder.dolly@gmail.com>
2021-12-11 04:24:50 +0530

    New project started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <button className='btn btn-info'>Hello</button>
       </header>
     </div>
   );
