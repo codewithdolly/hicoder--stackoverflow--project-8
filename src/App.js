c534190ee86abf2dd3945e25beb3dd44736652b9
hicoderindia <hicoder.dolly@gmail.com>
2021-12-11 21:01:50 +0530

    working on home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
-import logo from './logo.svg';
-import './App.scss';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "./App.scss";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import logo from "./Components/Images/logo1.png";
+import { HomePage } from "./Components";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <nav class="app navbar navbar-expand-lg navbar-light bg-light px-5">
+        <span class="navbar-toggler-icon mx-2 ml-5"></span>
+
+        <a class="navbar-brand" href="#">
+          <img src={logo} alt="" width="160px" />
         </a>
-        <button className='btn btn-info'>Hello</button>
-      </header>
-    </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item mx-4 active">
+              <a class="nav-link" href="#">
+                About <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item mr-4">
+              <a class="nav-link" href="#">
+                Products
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                For Turms
+              </a>
+            </li>
+          </ul>
+          <form class="form-inline mx-2 my-lg-0 mr-5">
+            <span class="fas fa-search search"></span>
+            <input
+              type="text"
+              class="form-control pr-5 pl-4 searchInput"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-sm"
+              placeholder="Search..."
+            />
+            <button
+              class="btn btn-outline-primary ml-2 my-sm-0 btn-sm px-2"
+              type="submit"
+              style={{ backgroundColor: "#e3f2fd", color: "gray" }}
+            >
+              Log in
+            </button>
+            <button
+              class="btn btn-primary mx-2 my-sm-0 btn-sm px-2"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </nav>
+      <HomePage />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./HomePage.scss";
+import secure from "../Images/secure.png";
+import search from "../Images/search.png";
+
+const HomePage = () => {
+  return (
+    <>
+      <div className="homePage">
+        <div className="homePage--innerDiv bg-dark text-light">
+          <div className="homePage--innerDiv--box align-middle">
+            <div className="homePage--innerDiv--box--find">
+              <img src={search} alt="search" />
+              <p>
+                Find the best answer to your technical question, help others
+                answer theirs
+              </p>
+              <button
+                type="button"
+                className="homePage--innerDiv--box--find--btn btn"
+              >
+                Join the Community
+              </button>
+              <br />
+              <small style={{ color: "gray" }}>
+                or{" "}
+                <a
+                  style={{ color: "black", textDecoration: "underline" }}
+                  href="#"
+                >
+                  search content
+                </a>
+              </small>
+            </div>
+            <div className="homePage--innerDiv--box--secure">
+              <img src={secure} alt="search" />
+              <p>Want a secure, private space for your technical knowledge?</p>
+
+              <p style={{ marginBottom: "-1rem" }}>Get started</p>
+              <hr text-primary />
+              <button type="button" className="btn btn-primary mr-2">
+                For large orginations<i class="fas fa-dice-d20"></i>
+              </button>
+              <button type="button" className="btn btn-primary">
+                For Small teams <i class="fas fa-user-friends"></i>
+              </button>
+            </div>
+          </div>
+          <h1>Every has a <b>mobile developerg</b> tab open to Stack Overflow</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
