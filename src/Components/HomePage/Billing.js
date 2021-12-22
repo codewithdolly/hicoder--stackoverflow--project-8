478aae9dc8fe996b4e707abd5d302d76d931fa8f
hicoderindia <hicoder.dolly@gmail.com>
2021-12-22 13:52:52 +0530

    Update Billing.js

--- src/Components/HomePage/Billing.js
+++ src/Components/HomePage/Billing.js
@@ -8,11 +8,11 @@ const Billing = () => {
   <input type="checkbox" class="custom-control-input p-4" id="customSwitch1" />
   <label class="custom-control-label text-light" for="customSwitch1">Annual billing discount</label>
 </div>
-      <div class="card " style={{ width: "18rem" }}>
+      <div class="card " style={{ width: "20rem" }}>
         <div class="card-body " align="left" >
         <button className="btn btn-warning px-4">Free</button>
-        <h2>Free</h2>
-          <p class="card-title">No credit card required</p>
+        <h4>Free</h4>
+          <p class="card-title  text-muted">No credit card required</p>
           <hr />
           <p class="card-text"><i class="fas fa-bullhorn mr-2"></i>
           ChatOps integrations - Slack & Microsoft Teams
@@ -26,7 +26,7 @@ const Billing = () => {
           <button class="btn btn-primary btn-block">
             Create a free team
           </button> <br />
-          <small className="text-muted">Always free up to 50 teammates</small>
+          <p className="text-muted"align="center"><small>Always free up to 50 teammates</small></p>
         </div>
         </div>
       </div>
