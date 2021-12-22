ca768814f991288a80f3273948220624e854dba0
hicoderindia <hicoder.dolly@gmail.com>
2021-12-22 13:46:46 +0530

    Update Billing.js

--- src/Components/HomePage/Billing.js
+++ src/Components/HomePage/Billing.js
@@ -3,21 +3,30 @@ import React from "react";
 const Billing = () => {
   return (
     <>
-    <div className="billing">
+    <div className="billing " >
     <div class="custom-control custom-switch m-5" align="left">
-  <input type="checkbox" class="custom-control-input" id="customSwitch1" />
-  <label class="custom-control-label text-light" for="customSwitch1">Toggle this switch element</label>
+  <input type="checkbox" class="custom-control-input p-4" id="customSwitch1" />
+  <label class="custom-control-label text-light" for="customSwitch1">Annual billing discount</label>
 </div>
-      <div class="card" style={{ width: "18rem" }}>
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+      <div class="card " style={{ width: "18rem" }}>
+        <div class="card-body " align="left" >
+        <button className="btn btn-warning px-4">Free</button>
+        <h2>Free</h2>
+          <p class="card-title">No credit card required</p>
+          <hr />
+          <p class="card-text"><i class="fas fa-bullhorn mr-2"></i>
+          ChatOps integrations - Slack & Microsoft Teams
           </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
+          <p class="card-text">
+          <i class="fas fa-award mr-2"></i>Your own private space hosted on stackoverflow.com
+          </p>
+          <p class="card-text">
+          <i class="fas fa-box mr-2"></i> Structured and searchable knowledge base
+          </p>
+          <button class="btn btn-primary btn-block">
+            Create a free team
+          </button> <br />
+          <small className="text-muted">Always free up to 50 teammates</small>
         </div>
         </div>
       </div>
