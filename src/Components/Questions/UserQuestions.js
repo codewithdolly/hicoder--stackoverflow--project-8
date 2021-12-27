7e70399a0c053bcf298f6788379fd3de6e264dbc
hicoderindia <hicoder.dolly@gmail.com>
2021-12-28 03:44:30 +0530

    Update UserQuestions.js

--- src/Components/Questions/UserQuestions.js
+++ src/Components/Questions/UserQuestions.js
@@ -3,14 +3,55 @@ import "./UserQuestions.scss";
 import { Link } from "react-router-dom";
 import img from "../Images/user6.png";
 
-const reviews = [112, 45, 65, 231, 251, 432, 63, 111,64, 53, 32, 421, 124, 321, 54];
+const hashtage = [
+    {
+        hash1: "excel",
+        hash2:"JavaScript",
+        hash3:"React",
+    },
+    {
+        hash1: "react",
+        hash2:"HTML",
+        hash3:"js",
+    },
+]
 
-const question = [];
+const questions = [
+    {
+        view: 111,
+        question:` Excel search multiples rows containing substring`,
+        ans:`I have an Excel file with 2 sheets : The first one got a list of
+        keywords in a column. The second one got sentences on a column along
+        with an id on another column. Thus the 2 sheets look like this : ...`,
+        hashtag1:"excel",
+        hashtag: "react",
+        time:" 1 min",
+        img:"../Images/user6.png",
+        user:"Dolly singh",
+        score:111,
+        badge:"9",
+    },
+    {
+        view:91,
+        question:` Excel search multiples rows containing substring`,
+        ans:`I have an Excel file with 2 sheets : The first one got a list of
+        keywords in a column. The second one got sentences on a column along
+        with an id on another column. Thus the 2 sheets look like this : ...`,
+        hashtag:"excel",
+        time:"1 min",
+        img:"../Images/user3.png",
+        user:"Dolly singh",
+        score:111,
+        badge:"9",
+    },
+
+];
 
 const UserQuestions = () => {
   return (
     <>
-      <div className="userQuestions d-flex" style={{ fontSize: "12px" }}>
+    {questions.map((que)=>{
+        return  <div className="userQuestions d-flex border-bottom py-2" style={{ fontSize: "12px" }}>
         <div className="left text-muted mr-3">
           <p className="">
             0<br />
@@ -18,20 +59,18 @@ const UserQuestions = () => {
           </p>
           <p className="">
             0<br />
-            <span>Answeres</span>
+            <span>answers</span>
           </p>
-          <small>10 views</small>
+          <small>{que.view} views</small>
         </div>
         <div className="right">
           <Link to="/">
             <h5 className="que font-weight-normal">
-              Excel search multiples rows containing substring
+              {que.question}
             </h5>
           </Link>
           <p>
-            I have an Excel file with 2 sheets : The first one got a list of
-            keywords in a column. The second one got sentences on a column along
-            with an id on another column. Thus the 2 sheets look like this : ...
+           {que.ans}
           </p>
           <button
             className="btn btn-sm mr-1"
@@ -41,7 +80,7 @@ const UserQuestions = () => {
               fontSize: "10px",
             }}
           >
-            excel
+            {que.hashtag}
           </button>
           <button
             className="btn btn-sm mr-1"
@@ -64,10 +103,10 @@ const UserQuestions = () => {
             excel-formula
           </button>
           <div className="profile float-right mt-4">
-            <span className="mb-1">asked 1 min ago</span>
+            <span className="mb-1">asked {que.time} ago</span>
             <div className="d-flex mr-3">
               <img
-                src={img}
+                src={process.env.PUBLIC_URL + que.img }
                 alt="User"
                 className="mr-2"
                 width="40px"
@@ -75,17 +114,18 @@ const UserQuestions = () => {
                 style={{ borderRadius: "1rem" }}
               />
               <div>
-                <Link to="/">Dolly singh</Link>
+                <Link to="/">{que.user}</Link>
                 <div className="text-muted">
-                  <b>111 </b>
-                  <span className="dot"></span> 9
+                  <b>{que.score} </b>
+                  <span className="dot"></span> {que.badge}
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <hr />
+    })}
+     
     </>
   );
 };
