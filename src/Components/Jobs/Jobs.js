80847f6bc2ec81698ef201fb878117aca3cc76b9
hicoderindia <hicoder.dolly@gmail.com>
2021-12-29 23:24:14 +0530

    Update Jobs.js

--- src/Components/Jobs/Jobs.js
+++ src/Components/Jobs/Jobs.js
@@ -4,51 +4,51 @@ import "./Jobs.scss"
 const Jobs = () => {
     return (
         <>
-            <div className='jobs'>
-                <nav class="navbar navbar-expand-lg navbar-light  mx-5 p-5 jobs--header">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+            <div classNameName='jobs'>
+                <nav className="navbar navbar-expand-lg navbar-light  mx-5 px-5 jobs--header">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Overview <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <a className="nav-link" href="#">Overview <span className="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Why Stack Overflow
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing <span class="sr-only">(current)</span></a>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Pricing <span className="sr-only">(current)</span></a>
       </li>
-            <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Resources
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Case Studies</a>
-          <a class="dropdown-item" href="#">Use Cases</a>
-          <a class="dropdown-item" href="#">Help Cewneter</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Calculaters</a>
-          <a class="dropdown-item" href="#">Reduce Support Ticket</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Resoure center</a>
-          <a class="dropdown-item" href="#">Videos and more</a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Case Studies</a>
+          <a className="dropdown-item" href="#">Use Cases</a>
+          <a className="dropdown-item" href="#">Help Cewneter</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Calculaters</a>
+          <a className="dropdown-item" href="#">Reduce Support Ticket</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Resoure center</a>
+          <a className="dropdown-item" href="#">Videos and more</a>
         </div>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <div className="my-2 my-lg-0">
+      <button className="btn btn-outline-success my-sm-0" type="submit">Search</button>
+    </div>
   </div>
 </nav>
                 </div>
