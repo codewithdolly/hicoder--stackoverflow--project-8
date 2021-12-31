bf71d272352ee83bdb2bf32956a4baef5d25177d
hicoderindia <hicoder.dolly@gmail.com>
2021-12-31 23:59:18 +0530

    Additional Products- ends completed

--- src/Components/HomePage/AdditionalProducts.js
+++ src/Components/HomePage/AdditionalProducts.js
@@ -7,17 +7,21 @@ import Logo2 from "../Images/logo2.png";
 import blue from "../Images/blue.png";
 import yellow from "../Images/yellow.png";
 import Orange from "../Images/orange.png";
-import lock from "../Images/lock.png"
+import lock from "../Images/lock.png";
 
 const AdditionalProducts = () => {
   return (
     <>
       <div className="d-flex justify-content-between additionalProducts">
+        {/* Additional products left side started */}
         <div>
           <img src={imgleft} alt="imgleft" width="400px" />
           <img src={Orange} alt="" width="200px" className="orangeImg" />
           <img src={blue} alt="" className="blueImg" />
         </div>
+        {/* Additional products left side ends */}
+
+        {/* Additional products Middle part stated */}
         <div className="justify-content-center additionalProducts--centerPart">
           <center>
             {" "}
@@ -33,6 +37,7 @@ const AdditionalProducts = () => {
             technologists…
           </h3>
 
+          {/* Cards started */}
           {cards.map((card) => {
             return (
               <div className=" additionalProducts--centerPart--card border m-3">
@@ -49,21 +54,40 @@ const AdditionalProducts = () => {
               </div>
             );
           })}
+          {/* Cards Ends */}
         </div>
+        {/* Additional products Middle part Ends */}
+
+        {/* Additional products Right side ends */}
         <div>
           <img src={imgright} alt="" width="400px" />
           <img src={yellow} alt="" width="" className="yellowImg" />
         </div>
       </div>
+      {/* Additional products Right side ends */}
+
+      {/* private community creation */}
       <hr />
       <div className="d-flex justify-content-center align-self-center mb-4">
-        <div><img src={lock} alt=""/></div>
-        <p className="mr-3 pt-4">Build a <b> private community</b> to share technical or non-technical knowledge.
-
-</p>
-<button className="btn btn-outline" style={{color:"rgb(55, 159, 239)",border: "1px solid rgb(55, 159, 239)"}}><b>Create a free team</b></button>
+        <div>
+          <img src={lock} alt="" />
         </div>
-        <hr />
+        <p className="mr-3 pt-4">
+          Build a <b> private community</b> to share technical or non-technical
+          knowledge.
+        </p>
+        <button
+          className="btn btn-outline"
+          style={{
+            color: "rgb(55, 159, 239)",
+            border: "1px solid rgb(55, 159, 239)",
+          }}
+        >
+          <b>Create a free team</b>
+        </button>
+      </div>
+      {/* private community creation */}
+      <hr />
     </>
   );
 };
