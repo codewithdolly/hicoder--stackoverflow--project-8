8bde56c0efa252587bf03798e456a96e55cc3b5a
hicoderindia <hicoder.dolly@gmail.com>
2022-01-01 05:13:31 +0530

    Navbar added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import "./About.scss";
+import { Link } from "react-router-dom";
 
 const About = () => {
-    return (
-        <div>
-            About
+  return (
+    <>
+      <div className="about">
+      {/* Mini navbar startes */}
+        <div className="d-flex justify-content-between mx-5 py-3">
+          <div>
+            <ul class="nav">
+              <li class="nav-item ">
+                <Link to="/" class="nav-link btn active about-miniNav px-3">
+                  About
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/" class="nav-link text-muted about-miniNav px-3"  >
+                  Leadership
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/" class="nav-link text-muted about-miniNav px-3"  >
+                  Careers
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/" class="nav-link text-muted about-miniNav px-3"  >
+                  Open position
+                </Link>
+              </li>
+              <li class="nav-item about-miniNav px-3">
+                <Link to="/" class="nav-link text-muted"  >
+                  Press
+                </Link>
+              </li>
+              <li class="nav-item about-miniNav px-3">
+                <Link to="/" class="nav-link text-muted"  >
+                  Research
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/" class="nav-link text-muted about-miniNav px-3"  >
+                  Contact us
+                </Link>
+              </li>
+              
+            </ul>
+          </div>
+          <div className="d-flex align-items-center">
+            <p className="pt-1">new here?</p>
+            <button className="btn btn-primary btn-sm ml-2 mb-2">
+              Take a tour
+            </button>
+          </div>
         </div>
-    )
-}
+         {/* Mini navbar Ends */}
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
