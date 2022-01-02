220d17d44d6348d5333108a684e08c6c6d92d21a
hicoderindia <hicoder.dolly@gmail.com>
2022-01-03 02:44:37 +0530

    Job page

--- src/Components/Jobs/JobRoles.js
+++ src/Components/Jobs/JobRoles.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
 import "./JobRoles.scss";
-import ReactJS from "./ReactJS"
-import FrontendJob from "./FrontendJob"
-import JavaScript from "./JavaScript"
+import job1 from "../Images/job1.png"
+import { Link } from 'react-router-dom';
 
 const JobRoles = () => {
     return (
         <>
             <div className='d-flex'>
-                <div>
-                    <ul>
-                        <li><Link to="/react" >React</Link></li>
-                        <li><Link to="/javascript" >JavaScript</Link></li>
-                        <li><Link to="/frontend" >Frontend</Link></li>
-                    </ul>
+              {/* Right Side started */}
+                <div className='d-flex'>
+                   <div className='' style={{width:"40%"}}>
+                       <img src={job1} alt="" width="60px" className='rounded'/>
+                   </div>
+                   <div><h5><Link to="/" className='font-weight-normal'>Experienced Frontend (GraphQL / React / Typescript) Developers</Link></h5>
+                   <p>Theorem, LLC • <span className='text-muted'> No office location</span></p>
+                   <div>
+                       <button className="btn btn-outline-primary">reactjs</button>
+                       <button className="btn btn-outline-primary">reactjs</button>
+                       
+                   </div>
+                   </div>
                 </div>
-                <div>
-                <Routes>
-          <Route path="react" element={<ReactJS />} />
-          <Route path="javascript" element={<JavaScript />} />
-          <Route path="frontend" element={<FrontendJob />} />
-        </Routes>
+
+                {/* Right Side started */}
+                <div className='ml-5'>
+                <h2>Experienced Frontend (GraphQL / React / Typescript) Developers</h2>
+
                 </div>
             </div>
         </>
