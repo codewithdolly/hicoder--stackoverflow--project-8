50dfe6c1e94d2bd514860aa7ef179e71e3542101
hicoderindia <hicoder.dolly@gmail.com>
2022-01-03 04:58:01 +0530

    job page

--- src/Components/Jobs/JobRoles.js
+++ src/Components/Jobs/JobRoles.js
@@ -3,24 +3,58 @@ import "./JobRoles.scss";
 import job1 from "../Images/job1.png"
 import { Link } from 'react-router-dom';
 
+let jobs=[
+    {
+        name:"Experienced Frontend (GraphQL / React / Typescript) Developers",
+        img:"../Images/job1.png",
+        place:"Theorem, LLC",
+        jobType:" No office location",
+        WorkType1:"reactjs",
+        WorkType2:"javascript",
+        WorkType3:"figma",
+        WorkType4:"typescript",
+        WorkType5:"redux",
+    },
+    {
+        name:"React Developer (Remote)",
+        img:"../Images/job1.png",
+        place:"Theorem, LLC",
+        jobType:" No office location",
+        WorkType1:"reactjs",
+        WorkType2:"javascript",
+        WorkType3:"figma",
+        WorkType4:"typescript",
+        WorkType5:"redux",
+    },
+]
+
 const JobRoles = () => {
     return (
         <>
-            <div className='d-flex'>
+            <div className='d-flex border'>
               {/* Right Side started */}
-                <div className='d-flex'>
-                   <div className='' style={{width:"40%"}}>
+                <div className=' border-right p-2'>
+                 {jobs.map((job)=>{
+                     return <>
+                     <div className='d-flex border-bottom my-2'>
+                     <div className='' style={{width:"40%"}}>
                        <img src={job1} alt="" width="60px" className='rounded'/>
                    </div>
                    <div><h5><Link to="/" className='font-weight-normal'>Experienced Frontend (GraphQL / React / Typescript) Developers</Link></h5>
                    <p>Theorem, LLC • <span className='text-muted'> No office location</span></p>
-                   <div>
-                       <button className="btn btn-outline-primary">reactjs</button>
-                       <button className="btn btn-outline-primary">reactjs</button>
-                       
+                   <div className='btns'>
+                   <button className="btn jobButtonColor">reactjs</button>
+                   <button className="btn jobButtonColor">JavaScript</button>
+                   <button className="btn jobButtonColor">Figma</button>
+                   <button className="btn jobButtonColor">Typescript</button>
+                   <button className="btn jobButtonColor">figma</button>  
+                   </div>
+                   <p>10d ago • <span className='text-muted'> Remote</span></p>
                    </div>
                    </div>
-                </div>
+                     </>
+                 })}
+</div>
 
                 {/* Right Side started */}
                 <div className='ml-5'>
